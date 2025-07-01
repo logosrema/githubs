58ca53fd11b6dbb024a1d598381a1c5cad65d1a9
logosrema <pneumalogoss@gmail.com>
2025-07-01 14:25:27 +0000

    new me

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 function getGreeting() {
     return "Hello, World!";
-    echo "This line will not execute"; // This line is unreachable
+    echo "This line will not execute before the other come to the code"; // This line is unreachable
     // This is a comment
 }
 
